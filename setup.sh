05843d3ffdd510bf13aa6e362fb2c3185c858511
Lino Bigatti <linobigatti@protonmail.com>
2023-01-24 12:20:29 -0300

    Add bedrock theme.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-stow -D $1-theme i3 bumblebee-status rofi picom redshift sakura vim
-stow $1-theme i3 bumblebee-status rofi picom redshift sakura vim
+stow -R --dotfiles -t $HOME $1-theme i3 bumblebee-status rofi picom redshift sakura vim
